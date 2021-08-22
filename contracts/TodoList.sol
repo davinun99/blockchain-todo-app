5b2d4066cca0945320ae5ac514a9234d661acf67
David <david.nunez@oktana.io>
2021-08-22 13:15:13 -0400

    Created taskCreation event and testing it

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -8,12 +8,18 @@ contract TodoList {
         bool completed;
     }
     mapping (uint => Task) public tasks;
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
     constructor() public {
         createTask("Check out dappuniversity.com");
     }
     function createTask( string memory _content ) public{
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
     
 }
